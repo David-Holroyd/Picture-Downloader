b291b20c89386607403adef4ac9fcef0b9c8d13c
David-Holroyd <100813251+David-Holroyd@users.noreply.github.com>
2022-04-04 16:36:59 -0400

    Update flask_pics.py

--- flask_pics.py
+++ flask_pics.py
@@ -10,24 +10,24 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def read_file():
-#     if request.method == 'POST':
-#         if 'file' not in request.files:
-#             flash('No file')
-#             return redirect(request.url)
-#         file = request.files['file']
-#         df = pd.read_csv(file, header=None)
-#         pic_list = df.values.tolist()
-#         doc = docx.Document()
-#         doc.add_heading('TEST!!!', 0)
-#         for pic in pic_list:
-#             print(pic[0])
-#             picture_obj = requests.get(fr'{pic[0]}', stream=True)
-#             picture_obj.raw.decode_content = True
-#             with open("pic_saveover.jpg", "wb") as f:
-#                 shutil.copyfileobj(picture_obj.raw, f)
-#             doc.add_picture('pic_saveover.jpg', width=Inches(2.8))
-#         doc.save('test.docx')
-#         return 'File Saved!'
+     if request.method == 'POST':
+         if 'file' not in request.files:
+             flash('No file')
+             return redirect(request.url)
+         file = request.files['file']
+         df = pd.read_csv(file, header=None)
+         pic_list = df.values.tolist()
+         doc = docx.Document()
+         doc.add_heading('TEST!!!', 0)
+         for pic in pic_list:
+             print(pic[0])
+             picture_obj = requests.get(fr'{pic[0]}', stream=True)
+             picture_obj.raw.decode_content = True
+             with open("pic_saveover.jpg", "wb") as f:
+                 shutil.copyfileobj(picture_obj.raw, f)
+             doc.add_picture('pic_saveover.jpg', width=Inches(2.8))
+         doc.save('test.docx')
+         return 'File Saved!'
 
     return '''
     <!doctype html>
